8fd15decdc05057b7d6b966d8b4fd2b2e1e1997a
S C Jayakody <samj.cj@gmail.com>
2023-07-22 18:50:23 +0530

    As

--- src/Notify.java
+++ src/Notify.java
@@ -61,7 +61,8 @@ public class Notify extends BroadcastReceiver{
          Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
          //create an unique notification id. Here it is done using random numbers
          int notification_id = (int)(Math.random()*(8000-1+1)+1);
-         Intent sintent=Intent.setClass(context, Salarm);
+         Intent sintent= new Intent(context);
+         sintent.setClass(context, Salarm.class);
          PendingIntent pendingIntent = PendingIntent.getBroadcast(
           context,
           10,
